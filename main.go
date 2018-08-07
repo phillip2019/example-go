2d84d0c7409bab6c8a6af625cab1141e94d67966
Shane <msn734506700@live.com>
2018-08-08 07:54:23 +0800

    point

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 )
 
 func main() {
-	fmt.Print("I am happy!")
+	// fmt.Print("I am happy!")
+	defer printStack()
+	f(18)
+	x := 1
+	p := &x
+	fmt.Println(*p)
+	*p = 2
+	fmt.Println(x)
+}
+
+func f(x int) {
+	fmt.Printf("f(%d)\n", x+0/(x-16))
+	defer fmt.Printf("defer %d\n", x)
+	f(x - 1)
+}
+
+func printStack() {
+	var buf [4096]byte
+	n := runtime.Stack(buf[:], false)
+	os.Stdout.Write(buf[:n])
 }
